0629df5793eff6fb018556a1a7228cf064e025e5
malburo <tongquocbao0605@gmail.com>
2020-06-25 12:52:50 +0700

    fix UI search header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import styles from './style.module.scss';
 import Avatar from 'components/Avatar';
-const Header = () => {
+
+const Header = props => {
+  const { avatarPictureUrl } = props;
   return (
     <header
       className={`${styles.header} d-flex align-items-center justify-content-between`}>
@@ -11,12 +13,18 @@ const Header = () => {
           <Col className="col-auto">
             <h3>Malburo</h3>
           </Col>
-          <Col className="col-auto">
-            <input type="text" />
+          <Col className={`${styles.search} col-auto`}>
+            <input type="text" placeholder="Tìm kiếm..." />
           </Col>
           <Col className="col-2">
             <div className="d-flex justify-content-between align-items-center">
-              <Avatar img="" size="small" />
+              <Avatar
+                img={
+                  'https://noidangsong.vn/files/uploads/fb1735058496563345/1526444239-tt_avatar_small.jpg'
+                }
+                size="small"
+                className={styles.avatar}
+              />
             </div>
           </Col>
         </Row>
