301df076e21c60711bc7feba1a431e2d37b92671
malburo <tongquocbao0605@gmail.com>
2020-06-29 01:17:21 +0700

    add local icon

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './style.module.scss';
+import like from 'assets/images/icons/like.svg';
+import unlike from 'assets/images/icons/unlike.svg';
+import dot from 'assets/images/icons/dot.svg';
+import comment from 'assets/images/icons/comment.svg';
+import message from 'assets/images/icons/message.png';
+import save from 'assets/images/icons/save.png';
+import setting from 'assets/images/icons/setting.svg';
 
 export function LikeIcon(props) {
   const { isLiked, handleLike, postId } = props;
   return (
     <img
-      src={
-        isLiked
-          ? 'https://image.flaticon.com/icons/svg/1076/1076984.svg'
-          : 'https://image.flaticon.com/icons/svg/1077/1077035.svg'
-      }
+      src={isLiked ? like : unlike}
       alt="LikeIcon"
       className={styles.icon}
       onClick={() => handleLike(postId)}
@@ -18,51 +21,21 @@ export function LikeIcon(props) {
 }
 
 export function CommentIcon(props) {
-  return (
-    <img
-      src="https://image.flaticon.com/icons/svg/2462/2462719.svg"
-      alt="CommentIcon"
-      className={styles.icon}
-    />
-  );
+  return <img src={comment} alt="CommentIcon" className={styles.icon} />;
 }
 
 export function MessageIcon(props) {
-  return (
-    <img
-      src="https://image.flaticon.com/icons/png/512/1932/1932893.png"
-      alt="MessageIcon"
-      className={styles.icon}
-    />
-  );
+  return <img src={message} alt="MessageIcon" className={styles.icon} />;
 }
 
 export function SaveIcon(props) {
-  return (
-    <img
-      src="https://image.flaticon.com/icons/png/512/942/942176.png"
-      alt="SaveIcon"
-      className={styles.icon}
-    />
-  );
+  return <img src={save} alt="SaveIcon" className={styles.icon} />;
 }
 
 export function DotIcon(props) {
-  return (
-    <img
-      src="https://image.flaticon.com/icons/svg/512/512142.svg"
-      alt="SaveIcon"
-      className={styles.icon}
-    />
-  );
+  return <img src={dot} alt="SaveIcon" className={styles.icon} />;
 }
 
 export function SettingIcon(props) {
-  return (
-    <img
-      src="https://image.flaticon.com/icons/svg/929/929577.svg"
-      alt="SettingIcon"
-      className={styles.icon}
-    />
-  );
+  return <img src={setting} alt="SettingIcon" className={styles.icon} />;
 }
--- src/features/Post/components/PostCard/index.jsx
+++ src/features/Post/components/PostCard/index.jsx
@@ -76,7 +76,9 @@ function PostCard(props) {
     <div className={styles['post-card']} style={{ backgroundColor: '#fff' }}>
       <div className={styles['post-card__header']}>
         <div className={styles.infoUser}>
-          <Avatar img={profilePictureUrl} size="small" />
+          <Link to={username}>
+            <Avatar img={profilePictureUrl} size="small" />
+          </Link>
           <Link to={username}>{username}</Link>
         </div>
         <DotIcon />
