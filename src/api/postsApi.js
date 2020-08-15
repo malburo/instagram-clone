58a7db9761337ac1237fd475d9b56ab5c9cb74c0
malburo <tongquocbao0605@gmail.com>
2020-08-16 02:29:21 +0700

    Add axiosClient.js and postApi.js

---
+++ src/api/postsApi.js
@@ -0,0 +1,25 @@
+const { default: axiosClient } = require('./axiosClient');
+
+const postsApi = {
+  get: () => {
+    const url = 'posts';
+    return axiosClient.get(url);
+  },
+  createPost: formData => {
+    const url = 'posts/create';
+    return axiosClient.post(url, formData);
+  },
+  like: postId => {
+    const url = 'likes/like';
+    return axiosClient.post(url, { postId });
+  },
+  unlike: postId => {
+    const url = 'likes/unlike';
+    return axiosClient.post(url, { postId });
+  },
+  comment: (postId, comment) => {
+    const url = 'comments/create';
+    return axiosClient.post(url, { postId, comment });
+  },
+};
+export default postsApi;
