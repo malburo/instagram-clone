82c0bbfd03f2f1b2da9672c2b1bc530e8f20302d
malburo <tongquocbao0605@gmail.com>
2020-08-22 21:08:26 +0700

    use authApi

--- src/features/Auth/pages/ResetPassword/index.jsx
+++ src/features/Auth/pages/ResetPassword/index.jsx
@@ -1,20 +1,28 @@
+import authApi from 'api/authApi';
 import ResetPasswordForm from 'features/Auth/components/ResetPasswordForm';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Col, Container, Row } from 'reactstrap';
 import styles from './style.module.scss';
 ResetPasswordPage.propTypes = {};
 
 function ResetPasswordPage(props) {
-  const handleSubmit = values => {
-    console.log(values);
+  const [isSuccess, setIsSucess] = useState(false);
+  const handleSubmit = async (email, actions) => {
+    try {
+      await authApi.resetPassword(email);
+      setIsSucess(true);
+    } catch (err) {
+      const { errors } = err.response.data;
+      actions.setErrors({ ...errors });
+    }
   };
   return (
     <Container fluid>
       <Row>
         <Col>
           <div className={styles['reset-page']}>
-            <ResetPasswordForm onSubmit={handleSubmit} />
+            <ResetPasswordForm onSubmit={handleSubmit} isSuccess={isSuccess} />
             <div className={styles['reset-page__nav']}>
               <Link to="/auth/login">Quay lại đăng nhập </Link>
             </div>
