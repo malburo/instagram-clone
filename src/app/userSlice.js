cb3f43a56cbeb44f81c0741aff4abf369a95c733
malburo <tongquocbao0605@gmail.com>
2020-08-26 21:44:36 +0700

    create userSlice.js, handle async action, save access token to local storage

--- src/app/userSlice.js
+++ src/app/userSlice.js
@@ -1,10 +1,26 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import userApi from 'api/userApi';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import authApi from 'api/authApi';
+import profileApi from 'api/profileApi';
+
+export const getMe = createAsyncThunk('user/getMe', async params => {
+  const currentUser = await authApi.getMe();
+  return currentUser;
+});
 
 export const login = createAsyncThunk(
   'user/login',
   async (params, thunkAPI) => {
-    const response = await userApi.login(params);
+    const response = await authApi.login(params);
+
+    // Save access token to local storage
+    const { access_token } = response;
+    localStorage.setItem('access_token', access_token);
+  }
+);
+export const register = createAsyncThunk(
+  'user/register',
+  async (params, thunkAPI) => {
+    const response = await authApi.register(params);
 
     // Save access token to local storage
     const { access_token } = response;
@@ -12,16 +28,11 @@ export const login = createAsyncThunk(
   }
 );
 
-export const getMe = createAsyncThunk('user/getMe', async params => {
-  const currentUser = await userApi.getMe();
-  return currentUser;
-});
-
 export const changeAvatar = createAsyncThunk(
   'user/changeAvatar',
-  async params => {
-    const profilePictureUrl = await userApi.changeAvatar();
-    return profilePictureUrl;
+  async (params, thunkAPI) => {
+    const response = await profileApi.changeAvatar(params);
+    return response.profilePictureUrl;
   }
 );
 const userSlice = createSlice({
@@ -48,49 +59,6 @@ const userSlice = createSlice({
   },
 });
 
-const { reducer: userReducer } = userSlice;
+const { reducer: userReducer, actions } = userSlice;
+export const { logout } = actions;
 export default userReducer;
-
-// export const signIn = createAsyncThunk(
-//   'user/login',
-//   async (params, thunkAPI) => {
-//     const response = await userApi.signIn(params);
-
-//     // Save access token to storage
-//     const { access_token, token_type, expired_at } = response;
-//     const accessToken = `${token_type} ${access_token}`;
-//     localStorage.setItem('access_token', accessToken);
-//     localStorage.setItem('expired_at', expired_at); // expired_at is a timestamp
-//   }
-// );
-// const user = createSlice({
-//   name: 'user',
-//   initialState: {
-//     current: {},
-//   },
-//   reducers: {
-//     loginSuccess: (state, action) => {
-//       state.user = action.payload;
-//       state.isAuthenticated = true;
-//     },
-//     logoutSuccess: (state, action) => {
-//       localStorage.removeItem('jwtToken');
-//     },
-//     changeAvatar: (state, action) => {
-//       state.user.profilePictureUrl = action.payload;
-//     },
-//     extraReducers: {
-//       [getMe.fulfilled]: (state, action) => {
-//         state.current = action.payload || {};
-//       },
-//       [getMe.rejected]: (state, action) => {
-//         state.current = {};
-//       },
-//     },
-//   },
-// });
-
-// const { reducer, actions } = user;
-// export const { loginSuccess, logoutSuccess, changeAvatar } = actions;
-// export default reducer;
-//  Thunk API
