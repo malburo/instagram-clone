48fa2292f60d9693707cef82a92be1cd38bb8539
malburo <tongquocbao0605@gmail.com>
2020-08-31 23:03:25 +0700

    Update upload image field

--- src/custom-field/UploadField/index.jsx
+++ src/custom-field/UploadField/index.jsx
@@ -1,8 +1,9 @@
-import { ErrorMessage } from 'formik';
+import { UploadOutlined } from '@ant-design/icons';
+import { Button, Upload } from 'antd';
+import Modal from 'antd/lib/modal/Modal';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { FormFeedback, FormGroup, Input } from 'reactstrap';
-
+import React, { useState } from 'react';
+import { getBase64 } from 'utils/common';
 UploadField.propTypes = {
   field: PropTypes.object.isRequired,
   form: PropTypes.object.isRequired,
@@ -23,19 +24,51 @@ function UploadField(props) {
   const { errors, setFieldValue } = form;
   const showError = !!errors[name];
 
+  const [fileList, setFileList] = useState([]);
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [previewTitle, setPreviewTitle] = useState('');
+  const beforeUpload = file => {
+    setFileList([...fileList, file]);
+    return false;
+  };
+
+  const handlePreview = async file => {
+    if (!file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.preview);
+    setPreviewVisible(true);
+    setPreviewTitle(file.name);
+  };
+  const handleChange = ({ fileList }) => {
+    setFileList(fileList);
+    setFieldValue('file', fileList);
+  };
+
+  const handleCancel = () => setPreviewVisible(false);
   return (
-    <FormGroup>
-      <Input
+    <div>
+      <Upload
         id={name}
-        onChange={event => {
-          setFieldValue('file', event.currentTarget.files[0]);
-        }}
         type={type}
-        disabled={disabled}
-        invalid={showError}
-      />
-      <ErrorMessage name={name} component={FormFeedback} />
-    </FormGroup>
+        listType="picture"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
+        beforeUpload={beforeUpload}>
+        <Button icon={<UploadOutlined />} style={{ marginBottom: 10 }}>
+          Chọn hình ảnh
+        </Button>
+      </Upload>
+      <Modal
+        visible={previewVisible}
+        title={previewTitle}
+        footer={null}
+        onCancel={handleCancel}>
+        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+      </Modal>
+    </div>
   );
 }
 
