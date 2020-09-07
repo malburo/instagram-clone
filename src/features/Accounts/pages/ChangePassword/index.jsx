0d6d58ea444453a9588628efbee6a12608d868fe
malburo <tongquocbao0605@gmail.com>
2020-09-07 19:25:33 +0700

    HandleSubmit ChangePassword & EditProfile

--- src/features/Accounts/pages/ChangePassword/index.jsx
+++ src/features/Accounts/pages/ChangePassword/index.jsx
@@ -1,12 +1,23 @@
 import ChangePasswordForm from 'features/Accounts/components/ChangePasswordForm';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { changePassword } from 'app/userSlice';
+import { unwrapResult } from '@reduxjs/toolkit';
 
-ChangePassword.propTypes = {};
 
 function ChangePassword(props) {
+  const dispatch = useDispatch();
+  const handleSubmit = async (values, action) => {
+    try {
+      const updateResult = await dispatch(changePassword(values));
+      unwrapResult(updateResult);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <ChangePasswordForm />
+      <ChangePasswordForm onSubmit={handleSubmit} />
     </div>
   );
 }
--- src/features/Accounts/pages/EditProfile/index.jsx
+++ src/features/Accounts/pages/EditProfile/index.jsx
@@ -1,14 +1,23 @@
-import { Menu } from 'antd';
-import EditProfileForm from 'features/Accounts/components/EditProfileForm';
 import ChangeAvatarForm from 'features/Accounts/components/ChangeAvatarForm';
+import EditProfileForm from 'features/Accounts/components/EditProfileForm';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './style.module.scss';
-const { SubMenu } = Menu;
+import { unwrapResult } from '@reduxjs/toolkit';
+import { editProfile } from 'app/userSlice';
 EditProfile.propTypes = {};
 
 function EditProfile(props) {
   const currentUser = useSelector(state => state.user.current);
+  const dispatch = useDispatch();
+  const handleSubmit = async (values, action) => {
+    try {
+      const updateResult = await dispatch(editProfile(values));
+      unwrapResult(updateResult);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className={styles.wrapper}>
       <ChangeAvatarForm
@@ -16,7 +25,7 @@ function EditProfile(props) {
         size="medium"
         currentUser={currentUser}
       />
-      <EditProfileForm currentUser={currentUser} />
+      <EditProfileForm currentUser={currentUser} onSubmit={handleSubmit} />
     </div>
   );
 }
